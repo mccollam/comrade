d09233637c8c7d58ab47de97f19cdc916c2215a6
Ronald McCollam <mccollam@gmail.com>
2016-07-01 17:32:06 -0400

    Don't loop on starting app

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-while true; do
-    rm /tmp/.X0-lock || true
+#while true; do
+#    rm /tmp/.X0-lock || true
     startx /app/node_modules/electron-prebuilt/dist/electron /app --enable-logging
-done
+#done
 
